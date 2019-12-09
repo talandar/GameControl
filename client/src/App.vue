e707bda87078f12c8359ed422eba5a1c211587eb
Talandar <jmtalandar@gmail.com>
2019-12-09 16:42:52 -0500

    start of work

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Ping msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Ping from './components/Ping.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Ping,
+  },
+};
+</script>
+
+<style>
+#app {
+  margin-top: 60px;
+}
+</style>
